15fb36c910460bc16ba23dd54bf9d5fca1550c0e
rayimovjonibek063-lab <rayimovjonibek063@gmail.com>
2026-04-16 00:08:53 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import joblib
 import plotly.express as px
 import plotly.graph_objects as go
 from datetime import datetime
-
 # 1. Sahifa konfiguratsiyasi
 st.set_page_config(page_title="Wildfire Intel AI | Jonibek Rayimov", layout="wide", page_icon="🔥")
 
